9907a93c0b499f6e9f55f309639548aaa1c13723
krasowskiM <krasowski.m87@gmail.com>
2018-05-26 16:20:09 +0200

    Add button visibility management

--- app/src/main/java/com/example/maciej_krasowski/lab1/ActivityLab1.java
+++ app/src/main/java/com/example/maciej_krasowski/lab1/ActivityLab1.java
@@ -2,8 +2,6 @@ package com.example.maciej_krasowski.lab1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +19,7 @@ public class ActivityLab1 extends AppCompatActivity {
 
     private void handleButton() {
         Button button = findViewById(R.id.submit_1);
+        button.setVisibility(View.INVISIBLE);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,13 +31,33 @@ public class ActivityLab1 extends AppCompatActivity {
 
     private void addTextChangeListeners() {
         final EditText nameText = findViewById(R.id.editText);
+        final Toast validationMessage = Toast.makeText(ActivityLab1.this, "Wprowadz wartosc z przedzialu pomiedzy 5 a 15", Toast.LENGTH_SHORT);
         nameText
                 .addTextChangedListener(new SimpleTextWatcher(nameText.getHint().toString(), ActivityLab1.this));
         EditText surnameText = findViewById(R.id.editText2);
         surnameText
                 .addTextChangedListener(new SimpleTextWatcher(surnameText.getHint().toString(), ActivityLab1.this));
-        EditText gradeText = findViewById(R.id.editText3);
-        surnameText
+        final EditText gradeText = findViewById(R.id.editText3);
+        gradeText
                 .addTextChangedListener(new SimpleTextWatcher(gradeText.getHint().toString(), ActivityLab1.this));
+        gradeText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                final String gradeValue = gradeText.getText().toString();
+                if (!gradeValue.isEmpty()) {
+                    int integerValue = Integer.parseInt(gradeValue);
+                    if (isInCorrectRange(integerValue)) {
+                        validationMessage.show();
+                    }
+                } else {
+                    validationMessage.show();
+                }
+            }
+        });
+
+    }
+
+    static boolean isInCorrectRange(int integerValue) {
+        return integerValue <= 5 || integerValue >= 15;
     }
 }
--- app/src/main/java/com/example/maciej_krasowski/lab1/SimpleTextWatcher.java
+++ app/src/main/java/com/example/maciej_krasowski/lab1/SimpleTextWatcher.java
@@ -1,9 +1,16 @@
 package com.example.maciej_krasowski.lab1;
 
 import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import static android.content.Context.POWER_SERVICE;
+import static com.example.maciej_krasowski.lab1.ActivityLab1.isInCorrectRange;
 
 
 /**
@@ -14,7 +21,7 @@ public class SimpleTextWatcher implements TextWatcher {
     private final String textFieldName;
     private final Context context;
 
-    public SimpleTextWatcher(String textFieldName, Context context) {
+    SimpleTextWatcher(String textFieldName, Context context) {
         this.textFieldName = textFieldName;
         this.context = context;
     }
@@ -26,23 +33,33 @@ public class SimpleTextWatcher implements TextWatcher {
 
     @Override
     public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        int idGradeNum = R.string.liczba_ocen;
-        String gradeText = context.getString(idGradeNum);
         if (charSequence.length() == 0) {
-            Toast toast =
-                    Toast.makeText(context,
-                            "Field " + textFieldName + " cannot be empty",
-                            Toast.LENGTH_SHORT);
+            Toast toast = Toast.makeText(context,
+                    "Pole " + textFieldName + " nie moze byc puste",
+                    Toast.LENGTH_SHORT);
             toast.show();
-        }
-        if(gradeText.equals(textFieldName)){
-            String value = charSequence.toString();
-
         }
     }
 
     @Override
     public void afterTextChanged(Editable editable) {
+        EditText name = ((AppCompatActivity) context).findViewById(R.id.editText);
+        EditText surname = ((AppCompatActivity) context).findViewById(R.id.editText2);
+        EditText grade = ((AppCompatActivity) context).findViewById(R.id.editText3);
+        Button submitButton = ((AppCompatActivity) context).findViewById(R.id.submit_1);
 
+        String nameValue = name.getText().toString();
+        String surnameValue = surname.getText().toString();
+        String gradeValue = grade.getText().toString();
+        if (!nameValue.isEmpty() && !surnameValue.isEmpty() && !gradeValue.isEmpty()) {
+            int integerGrade = Integer.parseInt(gradeValue);
+            if (isInCorrectRange(integerGrade)) {
+                submitButton.setVisibility(View.VISIBLE);
+            } else {
+                submitButton.setVisibility(View.INVISIBLE);
+            }
+        } else {
+            submitButton.setVisibility(View.INVISIBLE);
+        }
     }
 }
